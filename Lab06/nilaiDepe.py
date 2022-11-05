8a21a443015f1943f000be13331690498ecdad7e
vyrozen <72198786+vyrozen@users.noreply.github.com>
2022-11-05 13:56:15 +0700

    Update nilaiDepe.py

--- Lab06/nilaiDepe.py
+++ Lab06/nilaiDepe.py
@@ -1,7 +1,6 @@
-import math as m
 print(f"Selamat mencoba Program Pemeriksa Nilai Dek Depe!\n=================================================\nMasukkan kunci jawaban:")
 test, value, benar = [], None, 0
-nilai = lambda a, b : m.floor((a/b)*100) #a benar, b total kunjaw
+nilai = lambda a, b : (a//b)*100 #a benar, b total kunjaw
 while not value == "STOP":
     value = input(str())
     if not value in ['A', 'B', 'C', 'D', 'E', 'STOP'] : exit(f"\n{value}, ga sesuai kriteria tolol")
@@ -9,7 +8,7 @@ while not value == "STOP":
     test.append(value)
 if len(test) == 0 : exit(f"MINIMAL 1 KUNJAW")
 print(f"Masukkan jawaban kamu:")
-answer = [input(str()) for i in range (len(test))]
+answer = [str(input()) for i in range (len(test))]
 for i in range (len(test)) : 
     if test[i] == answer[i] : benar += 1
 if nilai(benar,len(test)) >= 85 : expression = "Selamat :D"
